eefcd652b8a5e1299e07d6e5869f8b7753aee7b1
Warahon roonnapai <eepw0211@gmail.com>
2018-03-31 12:58:10 +0700

    Add Age calculator and JS Stalker exercise

---
+++ Javascript/Age Calculator/myage.js
@@ -0,0 +1,5 @@
+var age= prompt("How old are you?") ;
+
+var dayinlife = age*365.25 ;
+
+alert(age+" year old, is mean " + dayinlife +" day your been alive.")
---
+++ Javascript/JS Stalker/js-stalker.js
@@ -0,0 +1,6 @@
+var firstname = prompt("What is your firstname?") ;
+//prompt("what is your name?")
+var lastname=prompt("What is your lastname?") ;
+var age=prompt("How old are you?") ;
+console.log( "User Full name is " + firstname + " " + lastname );
+console.log( "User are " + age +" Year old." );
