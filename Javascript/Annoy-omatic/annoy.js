dbd82a236d37c3a635aa4a24e804125f807baf18
Waraphon Roonnapai <eepw0211@gmail.com>
2018-04-05 19:49:46 +0700

    Add for loop exercise

--- Javascript/Annoy-omatic/annoy.js
+++ Javascript/Annoy-omatic/annoy.js
@@ -1,12 +1,15 @@
 var enter =prompt("Are we there yet?");
 
-while (enter !== "yes") {
+//while (enter !== "yes") {
+// 	enter = prompt("Are we there yet?");
+//}
+//alert("Finally,We made it!!");
+
+//better
+while (enter.indexOf("yes") === -1 && enter.indexOf("yeah") === -1) {
  	enter = prompt("Are we there yet?");
 }
-
 alert("Finally,We made it!!");
-
-
 
 
 
--- Javascript/For Loops Problem Set/loop.js
+++ Javascript/For Loops Problem Set/loop.js
@@ -1,39 +1,38 @@
 
 
 //print all number 10 to 40
-var num = 10;
-console.log("Print all numbers between 10-40")
-while(num <= 40){
-	console.log(num);
-	num ++;
+console.log("Print all numbers between -10-19")
+
+for (var i = -10; i <= 19; i++) {
+	console.log(i);
 }
+
 
 //Print all even number between 10-40
-var num = 10;
 console.log("Print all even numbers between 10-40")
-while(num <= 40){
-	if( num%2 == 0){
-		console.log(num);
+
+for(var i=10; i <=40 ;i++){
+	if(i%2 === 0){
+		console.log(i)
 	}
-	num ++;
 }
+
 
 //Print all odd number between 300-333
-var num = 300;
 console.log("Print all odd numbers between 300-333")
-while(num <= 333){
-	if( num%2 !== 0){
-		console.log(num);
+
+for(i=300; i<=333 ; i++){
+	if(i%2 !==0){
+		console.log(i)
 	}
-	num ++;
 }
 
+
 //Print all number divisible by both 5 and 3 between 5 and 50
-var num = 5;
 console.log("Print all number divisible by both 5 and 3 between 5 and 50")
-while(num <= 50){
-	if( num%3 == 0 && num%5 == 0){
-		console.log(num);
+
+for(i=5;i<=50;i++){
+	if(i%3 ===0 && i%5===0){
+		console.log(i)
 	}
-	num ++;
 }
